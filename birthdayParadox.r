ea5008f30d6d33760e7c14201ddc51518fd108ee
Camila Braz <camilabraz03@gmail.com>
2022-04-30 17:50:05 -0300

    Create birthdayParadox.r

---
+++ birthdayParadox.r
@@ -0,0 +1,28 @@
+# Libraries
+
+# Function that returns the probability of two individuals sharing birthdays, given a number of individuals and days
+#
+birthdayParadox <- function(n_individuals, n_time) {
+ return(p <- 1 - choose(n_time, n_time - n_individuals) * factorial(n_individuals) / n_time ^ n_individuals)
+}
+
+
+# Number of individuals
+n_individuals = 23
+
+# Number of days (365 = one year)
+n_time = 365
+
+# Calling function
+p = birthday(n_individuals, n_time)
+p =round(p, 3)*100
+
+
+# Read form keyboard
+n_individuals <- as.integer(readline(prompt = "Please enter a number for the individuals: "))
+n_time <- as.integer(readline(prompt = "Please enter a number for the time: "))
+
+p = birthday(n_individuals, n_time)
+p =round(p, 3)*100
+
+print(paste0(p, "%"))
